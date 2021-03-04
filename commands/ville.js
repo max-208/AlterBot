486d64d3cf7050483bdcfc43627a9ee69a11c1f4
maxime <maxime.daniel208@gmail.com>
2021-03-04 16:58:24 +0100

    modified ville command

--- commands/ville.js
+++ commands/ville.js
@@ -1,6 +1,5 @@
 var fs = require("fs");
 const Discord = require('discord.js');
-const { DH_CHECK_P_NOT_SAFE_PRIME } = require("constants");
 
 module.exports = {
     name: 'ville',
@@ -23,10 +22,9 @@ module.exports = {
                         joueurs[userid].PM = parseInt(joueurs[userid].PM) - prixAchat
                         var global = JSON.parse(fs.readFileSync("data/global.json"));
                         const idVille = Object.keys(global.villes).length + 1;
-                        console.log(idVille);
                         var jsonObj = {};
                         jsonObj[userid] = 1;
-                        global.villes[idVille] = { "nom": args[1], "proprietaire": userid, "province": 0, "infReligion": jsonObj, "infEconomie": jsonObj, "features": [] };
+                        global.villes[idVille] = { "nom": args.slice(1).join(" "), "proprietaire": userid, "province": 0, "infReligion": jsonObj, "infEconomie": jsonObj, "features": [] };
                         joueurs[userid].villes = joueurs[userid].villes.concat([idVille]);
                         var retour1 = JSON.stringify(joueurs, null, 2);
                         fs.writeFileSync('data/joueurs.json', retour1);
@@ -49,7 +47,7 @@ module.exports = {
             if (global.villes[args[1]] != undefined) {
                 if (global.villes[args[1]].proprietaire == message.author.id) {
                     if (args[2] != undefined) {
-                        global.villes[args[1]].nom = args[2];
+                        global.villes[args[1]].nom = args.slice(2).join(" ");
                         var retour2 = JSON.stringify(global, null, 2);
                         fs.writeFileSync('data/global.json', retour2);
                         message.react("✅");
@@ -110,124 +108,8 @@ module.exports = {
             console.log("4");
             message.reply("cette fonctionalité arrive bientot !")
 
-        } else if (args[0].match(/[Ll][Ii][Ss][Tt][Ee]?/)) {
-            var userid = message.author.id;
-            var idVille = 0;
-            if (args[1] != undefined) {
-                if (args[1].match(/<@!?(\d+)>/)) {
-                    userid = message.mentions.users.first();
-                } else {
-                    idVille = args[1];
-                }
-            }
-            var embed;
-            if (idVille != 0) {
-                var global = JSON.parse(fs.readFileSync("data/global.json"));
-                if (global.villes[idVille] != undefined) {
-                    embed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setTitle(':city_sunset: Info ville ' + idVille + " - " + global.villes[idVille].nom);
-
-                    embed.addField("nom", global.villes[idVille].nom, true);
-                    embed.addField("proprietaire", "<@" + global.villes[idVille].proprietaire + ">", true);
-
-                    if (global.villes[idVille].province == 0 || global.villes[idVille].province > Object.keys(global.provinces).length) {
-                        embed.addField("province", "aucune province", true);
-                    } else {
-                        embed.addField("province", global.villes[idVille].province + " - " + global.provinces[global.villes[idVille].province].nom, true);
-                    }
-
-                    var sommeReligion = 0;
-                    for (var i in global.villes[idVille].infReligion) {
-                        sommeReligion = sommeReligion + global.villes[idVille].infReligion[i];
-                    }
-                    var texteReligion = "\u200b";
-                    for (var i in global.villes[idVille].infReligion) {
-                        texteReligion = texteReligion + global.villes[idVille].infReligion[i] + " / " + sommeReligion + " - <@" + i + ">\n"
-                    }
-                    //console.log(texteReligion)
-                    embed.addField("Influence religieuse", texteReligion, true);
-
-
-                    var sommeEconomie = 0;
-                    for (var i in global.villes[idVille].infEconomie) {
-                        sommeEconomie = sommeEconomie + global.villes[idVille].infEconomie[i];
-                    }
-                    var texteEconomie = "\u200b";
-                    for (var i in global.villes[idVille].infEconomie) {
-                        texteEconomie = texteEconomie + global.villes[idVille].infEconomie[i] + " / " + sommeEconomie + " - <@" + i + ">\n"
-                    }
-                    //console.log(texteEconomie)
-                    embed.addField("Influence economique", texteEconomie, true);
-
-
-                    var param = JSON.parse(fs.readFileSync("data/param.json"));
-
-                    var texteFeatures = "\u200b";
-                    if (global.villes[idVille].features != undefined) {
-                        for (var i in global.villes[idVille].features) {
-                            var indexFeature = global.villes[idVille].features[i]
-                            if (param.feature[indexFeature] != undefined) {
-                                texteFeatures = texteFeatures + indexFeature + " - " + param.feature[indexFeature].nom + "\n"
-                            } else {
-                                texteFeatures = texteFeatures + indexFeature + " - \n"
-                            }
-                        }
-                    } else {
-                        texteFeatures = "aucune améliorations"
-                    }
-
-                    embed.addField("Ameliorations de la ville", texteFeatures, false);
-
-                    message.channel.send(embed);
-
-                } else {
-                    message.reply("deuxième argument incorrect : cette ville n'existe pas")
-                }
-            } else {
-                var joueurs = JSON.parse(fs.readFileSync("data/joueurs.json"));
-                if (joueurs[userid] != undefined) {
-                    embed = new Discord.MessageEmbed()
-                        .setColor('#0099ff')
-                        .setTitle('Info villes de ' + message.author.username);
-
-                    var global = JSON.parse(fs.readFileSync("data/global.json"));
-
-                    var dicTexte = { "0": "\u200b" };
-                    for (var i in joueurs[userid].provinces) {
-                        dicTexte[joueurs[userid].provinces[i]] = "\u200b";
-                    }
-
-                    for (var i in joueurs[userid].villes) {
-                        var indVille = parseInt(joueurs[userid].villes[i]);
-                        console.log(i + " - " + indVille);
-                        //la ligne au dessous bug :O
-                        dicTexte[global.villes[joueurs[userid].villes[i]].province] = dicTexte[global.villes[joueurs[userid].villes[i]].province] + joueurs[userid].villes[i] + " - " + global.villes[joueurs[userid].villes[i]].nom + "\n";
-                    }
-
-                    var titre = "\u200b"
-                    for (var i in dicTexte) {
-                        if (i == 0) {
-                            titre = "aucune province"
-                        } else if (global.provinces[i] != undefined) {
-                            titre = i + " - " + global.provinces[i].nom;
-                        } else {
-                            titre = i + " - ";
-                        }
-                        embed.addField(titre, dicTexte[i], true);
-                    }
-
-                    message.channel.send(embed);
-
-                } else {
-                    message.reply("deuxième argument incorrect : ce joueur ne participe pas au rp")
-                }
-
-            }
-
-
         } else {
-            message.reply("premier argument incorrect : valeurs attendues \n `buy, edit, improve, influence, list`");
+            message.reply("premier argument incorrect : valeurs attendues \n `buy, edit, improve, influence`");
         }
     },
 };
