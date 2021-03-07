f894b2a5852b97aa0e320df55b94c355e56dd583
maxime <maxime.daniel208@gmail.com>
2021-03-07 12:25:54 +0100

    edited bataille command to take into account long battles and draws

--- commands/bataille.js
+++ commands/bataille.js
@@ -94,18 +94,20 @@ module.exports = {
             min = 0;
             max = (forceJ1 + forceJ2) * difficulte;
             equilibre = max / 100 * equilibre;
-            console.log(J1);
-            console.log(J1);
-            messagePublic = await message.channel.send("__**partie entre " + J1.username + "(j1) et " + J2.username + "(j2).**__");
+            messagePublic = await message.channel.send("__**partie entre " + J1.username + "(j1) et " + J2.username + "(j2).**__, " + (10 * difficulte) + " round max");
             var round = 1;
+            var choixJ1;
+            var choixJ2;
 
-            while (round <= 9 && equilibre > (max/100*10) && equilibre < (max - (max/100*10))) {
+            while (round <= (10*difficulte) && (choixJ1 !== 4 || choixJ2 !== 4) && equilibre > (max/100*10) && equilibre < (max - (max/100*10))) {
 
-                var choixJ1 = 0;
-                var choixJ2 = 0;
+                choixJ1 = 0;
+                choixJ2 = 0;
                 messagej1 = await J1.send('initialisation de la partie contre ' + J2.username);
                 messagej2 = await J2.send('initialisation de la partie contre ' + J1.username);
-                //idee : faire barre de progression avec █ ▓ ▒ ░ █
+                if(round%5 == 0){
+                    messagePublic = await message.channel.send("__**partie entre " + J1.username + "(j1) et " + J2.username + "(j2).**__, " + (10 * difficulte) + " round max");
+                }
                 var text = "j1🔷";
                 for (var i = (max/100*10); i < (max - (max/100*10)); i = i + (max/18)){
                     if(i < equilibre) {
@@ -116,36 +118,41 @@ module.exports = {
                 }
                 text = text + "🔶j2";
                 await messagePublic.edit(messagePublic.content + "\n\n__round " + round + " - " + equilibre + " / " + max + "__\n" + text);
-                console.log("round " + round + " longeur " + messagePublic.content.length)
+                //console.log("round " + round + " longeur " + messagePublic.content.length)
                 
                 await Promise.all([
                     (async () => choixJ1 = await pfc(messagej1, round, J2.username, (equilibre + " / " + max + " , atteignez " + (max - (max/100*10)) + " pour gagner\n" + text)))(),
                     (async () => choixJ2 = await pfc(messagej2, round, J1.username, (equilibre + " / " + max + " , atteignez " + (max/100*10) + " pour gagner\n"+text)))()
                 ])
-                var arr = ["🔇", "⛰️", "📜", "⚔️"];
+                var arr = ["🔇", "⛰️", "📜", "⚔️", "❌" ];
                 await messagePublic.edit(messagePublic.content + "\n"  + arr[choixJ1] + " vs " + arr[choixJ2]);
                 await messagej1.edit(messagej1.content + "\n" + arr[choixJ1] + " vs " + arr[choixJ2] );
                 await messagej2.edit(messagej2.content + "\n" + arr[choixJ1] + " vs " + arr[choixJ2] );
-                console.log(arr[choixJ1] + " vs " + arr[choixJ2]);
+                //console.log(arr[choixJ1] + " vs " + arr[choixJ2]);
 
-                if (choixJ1 == 0 || choixJ2 == 0) {
-                    console.log("erreur pas de reponse");
+                if (choixJ1 == -1 || choixJ2 == -1) {
+                    //console.log("erreur pas de reponse");
+                    await messagePublic.edit(messagePublic.content+  " un des participant shouaite arreter");
+                    messagej1.edit( messagej1.content + "\nun des participant **shouaite arreter**");
+                    messagej2.edit( messagej2.content + "\nun des participant **shouaite arreter**");
+                } else if (choixJ1 == 0 || choixJ2 == 0) {
+                    //console.log("erreur pas de reponse");
                     await messagePublic.edit(messagePublic.content+  " erreur: un des participants n'a pas répondu");
-                    messagej1.edit( messagej1.content + "\nerreur: un des participants n'a pas répondu");
-                    messagej2.edit( messagej2.content + "\nerreur: un des participants n'a pas répondu");
+                    messagej1.edit( messagej1.content + "\nerreur: un des participants **n'a pas répondu**");
+                    messagej2.edit( messagej2.content + "\nerreur: un des participants **n'a pas répondu**");
                 } else if (choixJ1 == choixJ2) {
-                    console.log("equalité");
+                    //console.log("equalité");
                     await messagePublic.edit(messagePublic.content + " egalité");
                     messagej1.edit( messagej1.content + "\n**egalité**");
                     messagej2.edit( messagej2.content + "\n**egalité**");
                 } else if( (choixJ1 == 1 && choixJ2 == 3) || (choixJ1 == 2 && choixJ2 == 1) || (choixJ1 == 3 && choixJ2 == 2)) {
-                    console.log("victoire j1");
+                    //console.log("victoire j1");
                     await messagePublic.edit(messagePublic.content + " victoire de **J1**");
                     messagej1.edit( messagej1.content + "\n**victoire**");
                     messagej2.edit( messagej2.content + "\n**echec**");
                     equilibre = await equilibre + forceJ1;
                 } else {
-                    console.log("victoire j2");
+                    //console.log("victoire j2");
                     await messagePublic.edit(messagePublic.content + " victoire de **J2**");
                     messagej1.edit( messagej1.content + "\n**echec**");
                     messagej2.edit( messagej2.content + "\n**victoire**");
@@ -164,25 +171,30 @@ module.exports = {
                 await messagePublic.edit(messagePublic.content + "\n victoire de <@" + J1.id + ">");
                 await J2.send("echec :(");
                 await J1.send("victoire !");
-            } else {
+            } else if(round > (10 * difficulte)){
                 await messagePublic.edit(messagePublic.content + "\n limite de tours dépassée : égalité");
                 await J1.send("limite de tours dépassée : égalité");
                 await J2.send("limite de tours dépassée : égalité");
+            } else {
+                await messagePublic.edit(messagePublic.content + "\n limite de tours dépassée : égalité");
+                await J1.send("deux abandons : égalité");
+                await J2.send("deux abandons : égalité");
             }
         }
 
         async function pfc(message, round, nomEnnemi, texteEquilibre) {
-            await message.edit("**combat contre " + nomEnnemi + "**\n__round " + round + " - " + texteEquilibre + "__\nréagissez pierre, feuille ou ciseaux pour faire votre mouvement");
+            await message.edit("**combat contre " + nomEnnemi + "**\n__round " + round + " - " + texteEquilibre + "__\nréagissez ⛰️, 📜 ou ⚔️ pour faire votre mouvement. \nréagissez ❌ pour demander d'arrêter le combat (paix blanche)");
             await message.react("⛰️");
             await message.react("📜");
             await message.react("⚔️");
+            await message.react("❌");
             const filter = (reaction, user) => {
-                return ["⛰️", "📜", "⚔️"].includes(reaction.emoji.name);
+                return ["⛰️", "📜", "⚔️", "❌"].includes(reaction.emoji.name);
             };
             var ret = 0
             await message.awaitReactions(filter, { max: 1, time: 300000, errors: ['time'] })
                 .then(collected => {
-                    console.log("react");
+                    //console.log("react");
                     const reaction = collected.first();
 
                     if (reaction.emoji.name === '⛰️') {
@@ -191,10 +203,12 @@ module.exports = {
                         ret = 2;
                     } else if (reaction.emoji.name === '⚔️') {
                         ret = 3;
+                    } else if (reaction.emoji.name === '❌') {
+                        ret = 4;
                     }
                 })
                 .catch(collected => {
-                    console.log("pas react");
+                    //console.log("pas react");
                     ret = 0;
                 });
             return ret;
