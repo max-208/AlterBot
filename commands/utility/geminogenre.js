2c8ffbe490275bd8f1810c863905620000620e35
Maxime DANIEL <maxime.daniel208@gmail.com>
2021-10-28 01:10:48 +0200

    ajout possibilité de changer url

--- commands/utility/geminogenre.js
+++ commands/utility/geminogenre.js
@@ -4,14 +4,21 @@ module.exports = {
 	name: 'geminogenre',
 	description: '',
 	args: false, 				//mettre a true quand la commande nécésite des arguments
-	//usage: '<arg1> <arg2>',	//décrit les arguments nécéssaires a la commande
+	usage: '[lien image]',	//décrit les arguments nécéssaires a la commande
 	guildOnly: true,			//définit si la commande doit etre utilisé seulement sur le serveur
 	cooldown: 5,				//cooldown en nombres de secondes
 	aliases: ["gemino"],	//autres manières d'appeler la commande
 	async execute(message, args) {
+		let lien =  message.author.avatarURL({format : "png",size : 512});
+		if(args.length > 0){
+			lien = args[0];
+		}
+		if(message.attachments.array().length > 0){
+			lien = message.attachments.array()[0].url;
+		}
         const canvas = Canvas.createCanvas(512, 512);
         const context = canvas.getContext('2d');
-        let avatar = await Canvas.loadImage( message.author.avatarURL({format : "png",size : 512}));
+        let avatar = await Canvas.loadImage(lien);
         let gemino = await Canvas.loadImage("https://media.discordapp.net/attachments/652186356231045120/901939614334468226/gemino.png");
         context.drawImage(avatar,0,0,canvas.width, canvas.height);
         context.drawImage(gemino,0,0,canvas.width, canvas.height);
