7579701537e28a7558fb54f441d605153657fe5b
Maxime DANIEL <maxime.daniel208@gmail.com>
2022-03-20 00:27:19 +0100

    protection avant avril

--- slash/avril/score.js
+++ slash/avril/score.js
@@ -1,7 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
 const data = require('data');
-
+const utilities = require('../../utilities');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -13,66 +13,70 @@ module.exports = {
      * @param {Discord.CommandInteraction} interaction 
      */
 	async execute(interaction) {
-		let user = interaction.user;
-		if(interaction.options.getUser("utilisateur") != undefined){
-			user = interaction.options.getUser("utilisateur");
-		}
-		let embed = new Discord.MessageEmbed();
-		embed.setTitle(" -- Crédit social de " + user.username + " -- ");
-		embed.setThumbnail(user.avatarURL());
-		await data.premierAvril_dao.getUser(user.id).then(function ret(res) {
-			if(res == undefined){
-				embed.addField("🔴 Crédit social","pas encore traqué",true)
-				embed.addField("💪 Puissance du vote","encore inconnue",true)
-			} else {
-				embed.addField("🔴 Crédit social","\u200b"+res.UserScore,true)
-				embed.addField("💪 Puissance du vote","\u200b"+res.Weight,true)
-				if(res.UserScore > 0){
-					embed.setColor("GREEN");
+		if(utilities.premierAvril == "TRUE"){
+			let user = interaction.user;
+			if(interaction.options.getUser("utilisateur") != undefined){
+				user = interaction.options.getUser("utilisateur");
+			}
+			let embed = new Discord.MessageEmbed();
+			embed.setTitle(" -- Crédit social de " + user.username + " -- ");
+			embed.setThumbnail(user.avatarURL());
+			await data.premierAvril_dao.getUser(user.id).then(function ret(res) {
+				if(res == undefined){
+					embed.addField("🔴 Crédit social","pas encore traqué",true)
+					embed.addField("💪 Puissance du vote","encore inconnue",true)
+				} else {
+					embed.addField("🔴 Crédit social","\u200b"+res.UserScore,true)
+					embed.addField("💪 Puissance du vote","\u200b"+res.Weight,true)
+					if(res.UserScore > 0){
+						embed.setColor("GREEN");
+					}
+					if(res.UserScore < 0){
+						embed.setColor("RED");
+					}
 				}
-				if(res.UserScore < 0){
-					embed.setColor("RED");
+			})
+			embed.addField("\u200b","\u200b")
+			let text = "\u200b";
+			let lastVotes = await data.premierAvril_dao.getLastSentVotes(user.id);
+			console.log(lastVotes);
+			for(let vote of lastVotes){
+				text = text + "[";
+				if(vote.voteScore > 0){
+					text = text + "+"
 				}
+				text = text + vote.voteScore + " donné a  " 
+				+ (await interaction.client.users.cache.get(vote.voteReciever)).username 
+				+ "](https://discord.com/channels/" 
+				+ interaction.guild.id + "/"
+				+ vote.voteChannel + "/"
+				+ vote.voteMessage
+				+")\n";
 			}
-		})
-		embed.addField("\u200b","\u200b")
-		let text = "\u200b";
-		let lastVotes = await data.premierAvril_dao.getLastSentVotes(user.id);
-		console.log(lastVotes);
-		for(let vote of lastVotes){
-			text = text + "[";
-			if(vote.voteScore > 0){
-				text = text + "+"
+			embed.addField("📤 Derniers points donnés : ", text)
+	
+	
+			text = "\u200b";
+			lastVotes = await data.premierAvril_dao.getLastRecievedVotes(user.id);
+			console.log(lastVotes);
+			for(let vote of lastVotes){
+				text = text + "[";
+				if(vote.voteScore > 0){
+					text = text + "+"
+				}
+				text = text + vote.voteScore + " donné par "
+				+ (await interaction.client.users.cache.get(vote.voteUser)).username 
+				+ "](https://discord.com/channels/" 
+				+ interaction.guild.id + "/"
+				+ vote.voteChannel + "/"
+				+ vote.voteMessage
+				+")\n";
 			}
-			text = text + vote.voteScore + " donné a  " 
-			+ (await interaction.client.users.cache.get(vote.voteReciever)).username 
-			+ "](https://discord.com/channels/" 
-			+ interaction.guild.id + "/"
-			+ vote.voteChannel + "/"
-			+ vote.voteMessage
-			+")\n";
+			embed.addField("📥 Derniers points reçus : ", text,true)
+			return interaction.reply({embeds : [embed]});
+		} else{
+			return interaction.reply("quelle mysterieuse commande, je me demande ce qu'elle fait, attendons le premier avril pour voir");
 		}
-		embed.addField("📤 Derniers points donnés : ", text)
-
-
-		text = "\u200b";
-		lastVotes = await data.premierAvril_dao.getLastRecievedVotes(user.id);
-		console.log(lastVotes);
-		for(let vote of lastVotes){
-			text = text + "[";
-			if(vote.voteScore > 0){
-				text = text + "+"
-			}
-			text = text + vote.voteScore + " donné par "
-			+ (await interaction.client.users.cache.get(vote.voteUser)).username 
-			+ "](https://discord.com/channels/" 
-			+ interaction.guild.id + "/"
-			+ vote.voteChannel + "/"
-			+ vote.voteMessage
-			+")\n";
-		}
-		embed.addField("📥 Derniers points reçus : ", text,true)
-		return interaction.reply({embeds : [embed]});
 		
 	},
 };
--- slash/avril/scoreboard.js
+++ slash/avril/scoreboard.js
@@ -1,6 +1,7 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 const Discord = require('discord.js');
 const data = require('data');
+const utilities = require('../../utilities');
 
 
 module.exports = {
@@ -13,23 +14,27 @@ module.exports = {
      * @param {Discord.CommandInteraction} interaction 
      */
 	async execute(interaction) {
-		let page = 0;
-		if(interaction.options.getInteger("page") != undefined && interaction.options.getInteger("page") > 0){
-			page = interaction.options.getInteger("page") - 1;
+		if(utilities.premierAvril == "TRUE"){
+			let page = 0;
+			if(interaction.options.getInteger("page") != undefined && interaction.options.getInteger("page") > 0){
+				page = interaction.options.getInteger("page") - 1;
+			}
+			let list = await data.premierAvril_dao.getScoreboard(page);
+	
+			let embed = new Discord.MessageEmbed();
+			embed.setTitle(" -- ScoreBoard - page " + (page+1) + " -- ");
+			let i = 0
+			for(let element of list){
+				i++;
+				let pos = page * 10 + i;
+				let user = await interaction.client.users.fetch(element.IdUser)
+				embed.addField(pos + " - " + user.username,"Crédit social : "+element.UserScore);
+			}
+			embed.setColor("BLUE");
+			interaction.reply({ embeds: [embed] });
+		} else {
+			return interaction.reply("quelle mysterieuse commande, je me demande ce qu'elle fait, attendons le premier avril pour voir");
 		}
-		let list = await data.premierAvril_dao.getScoreboard(page);
-
-		let embed = new Discord.MessageEmbed();
-		embed.setTitle(" -- ScoreBoard - page " + (page+1) + " -- ");
-		let i = 0
-		for(let element of list){
-			i++;
-			let pos = page * 10 + i;
-			let user = await interaction.client.users.fetch(element.IdUser)
-			embed.addField(pos + " - " + user.username,"Crédit social : "+element.UserScore);
-		}
-		embed.setColor("BLUE");
-		interaction.reply({ embeds: [embed] });
 		
 	},
 };
