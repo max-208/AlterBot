894d5c47b1714aaf6d680794fafadeeaa4ef1f48
awing_ding <awingding.win@gmail.com>
2022-12-05 14:11:57 +0100

    better subcommand readibility

--- slash/linguistique/translitterate.js
+++ slash/linguistique/translitterate.js
@@ -6,7 +6,7 @@ module.exports = {
         .setName('translitterate')
         .setDescription("permet de changer d'alphabet un mot ou une expression")
         .addSubcommand(subcommand => 
-                subcommand.setName("lattogeorg")
+                subcommand.setName("lat_to_georg")
                           .setDescription("translittère de l'alphabet latin vers l'alphabet georgien")
                           .addStringOption(option => 
                                 option.setName('text')
@@ -15,7 +15,7 @@ module.exports = {
                             )
             )
         .addSubcommand(subcommand => 
-            subcommand.setName("lattocyr")
+            subcommand.setName("lat_to_cyr")
                       .setDescription("translittère de l'alphabet latin vers l'alphabet cyrillique")
                       .addStringOption(option => 
                             option.setName('text')
@@ -24,7 +24,7 @@ module.exports = {
                         )
         )
         .addSubcommand(subcommand => 
-            subcommand.setName("georgtolat")
+            subcommand.setName("georg_to_lat")
                       .setDescription("translittère de l'alphabet georgien vers l'alphabet latin")
                       .addStringOption(option => 
                             option.setName('text')
@@ -33,7 +33,7 @@ module.exports = {
                         )
         )
         .addSubcommand(subcommand => 
-            subcommand.setName("cyrtolat")
+            subcommand.setName("cyr_to_lat")
                       .setDescription("translittère de l'alphabet cyrillique vers l'alphabet latin")
                       .addStringOption(option => 
                             option.setName('text')
@@ -45,7 +45,7 @@ module.exports = {
         async execute(interaction){
             let text = interaction.options.getString('text');
             let result = "";
-            if (interaction.options.getSubcommand() === 'lattogeorg'){
+            if (interaction.options.getSubcommand() === 'lat_to_georg'){
                 text = text.toLowerCase();
                 text = text.replace(/ts/, data.alphabet.georgian[0].ts).replace(/dz/, data.alphabet.georgian[0].dz).replace(/tš/, data.alphabet.georgian[0].tš).replace(/dž/, data.alphabet.georgian[0].dž);
                 for (const iterator of text) {
@@ -58,7 +58,7 @@ module.exports = {
                 }
                 await interaction.reply(result);
             }
-            else if (interaction.options.getSubcommand() === 'lattocyr'){
+            else if (interaction.options.getSubcommand() === 'lat_to_cyr'){
                 text = text.replace(/ts/, data.alphabet.cyrilic[0].ts).replace(/dz/, data.alphabet.cyrilic[0].dz).replace(/tš/, data.alphabet.cyrilic[0].tš).replace(/dž/, data.alphabet.cyrilic[0].dž);
                 for (const iterator of text) {
                     if (data.alphabet.cyrilic[0][iterator.toLowerCase()] != undefined) {
@@ -74,7 +74,7 @@ module.exports = {
                 }
                 await interaction.reply(result);
             }
-            else if (interaction.options.getSubcommand() === 'georgtolat'){
+            else if (interaction.options.getSubcommand() === 'georg_to_lat'){
                 text = text.replace(/\u10d3\u10ff/, 'ð').replace(/\u10e2\u10ff/, 'þ').replace(/\u10d0\u10fc/, 'ā').replace(/\u10dd\u10fc/, 'ō').replace(/\u10e3\u10fc/, 'ū');
                 for (const iterator of text) {
                     if (data.alphabet.georgian[1][iterator] != undefined){
@@ -86,7 +86,7 @@ module.exports = {
                 }
                 await interaction.reply(result);
             }
-            else if (interaction.options.getSubcommand() == 'cyrtolat'){
+            else if (interaction.options.getSubcommand() == 'cyr_to_lat'){
                 for (const iterator of text){
                     if (data.alphabet.cyrilic[1][iterator.toLowerCase()] != undefined) {
                         if (iterator.toUpperCase() == iterator) {
