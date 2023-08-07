98a163604bd629a6d0a4bb09c6b27abb52bb3bdb
awing_ding <awingding.win@gmail.com>
2023-08-07 22:18:21 +0200

    phonetic_based trame + test

--- data/translitterate.js
+++ data/translitterate.js
@@ -2,28 +2,83 @@ const alphabet = require('./alphabet.json');
 
 class translitterate {
     constructor() {
-        this.trame = (text) => {
-            const voyelles = /[aeéëioòuü]/;
-            const consonnes = /[bdfghkjlmnprřsštwvyzžðþ]/;
-            const nasale = /[āōū]/;
+        this.trame = (str) => {
+            const voyelles = /[aeɛøioɔuy]/;
+            const consonnes = /[bdfɡʔkjlmnn̪pʁɻsʃtwvzʒðθ]/;
+            const nasale = /\u0303/;
             let trame = [];
-            for (let i = 0; i < text.length; i++) {
-                const iterator = text[i];
-                if ((iterator == "j" || iterator == "w") && i < text.length - 1 && voyelles.test(text[i + 1])) {
-                    trame.push("jv");
-                    text = text.slice(0, i + 1) + text.slice(i + 2);
+            let list = str.split(" ")
+            for (let text of list) {
+                let word = []
+                for (let i = 0; i < text.length; i++) {
+                    const iterator = text[i];
+                    if ((iterator == "j" || iterator == "w") && i < text.length - 1 && voyelles.test(text[i + 1])) {
+                        if (nasale.test(text[i + 1])) {
+                            word.push("jvn");
+                        }
+                        else {
+                            word.push("jv");
+                        }
+                        text = text.slice(0, i + 1) + text.slice(i + 2);
+                        
+                    }
+                    else {
+                        word.push(iterator.replace(consonnes, "c").replace(voyelles, "v").replace(nasale, "n"));
+                    }
                 }
-                else {
-                    trame.push(iterator.replace(consonnes, "c").replace(voyelles, "v").replace(nasale, "vn"));
-                }
+                trame.push(word);
             }
             return trame;
+        };
+        this.syllabes = (trame) => {
+            let syllabes = [];
+            for (const word of trame) {
+                let syllabic_word = [];
+                let last_consonnant = 0;
+                for (let i = 0; i < word.length; i++) {
+                    let syllabe = [];
+                    let has_been_pushed = false;
+                    if (word[i] == "v" && i == 0) {
+                        syllabe.push("no_consonnant");
+                        syllabe.push("v");
+                        has_been_pushed = true;
+                    }
+                    if (word[i] == "v" && word[i + 1] == "v") {
+                        if (!has_been_pushed) syllabe.push("v"); has_been_pushed = true;
+                        syllabe.push("no_consonnant")
+                        syllabic_word.push(syllabe);
+                        syllabe = [];
+                    }
+                    else if (word[i] == "v" && syllabe.length !=0 && word[i + 1] == "c") {
+                        syllabe.push("v");
+                    }
+                    if (word[i] == "c" && word[i + 1] == "c" && syllabe.length !=0 ){
+                        syllabe.push('c');
+                        last_consonnant += 1;
+                        if (last_consonnant == 3) {
+                            syllabic_word.push(syllabe);
+                            syllabe = [];
+                            last_consonnant = 0;
+                        }
+                    }
+                    if (word[i] == "c" && word[i + 1] == "c" && syllabe.length == 0) {
+                        syllabe.push('c');
+                        syllabe.push('no_vowel');
+                    }
+                    }
+                    syllabes.push(syllabic_word);
+                }
+                return syllabes;
         };
         this.lat_to_kor = (text) => {
             let result = "";
             let trame = this.trame(text);
-            for (let i = 0; i < trame.length; i++) {
+            let j = 0;
+            for (let word of trame){
+                for (let i = 0; i < trame.length; i++) {
 
+                    j++
+                }
             }
         }
         this.lat_to_georg = (text) => {
