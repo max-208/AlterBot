080fd03f483fe45fa66501fc2f6ae843a6739eda
awing_ding <awingding.win@gmail.com>
2023-08-10 15:06:48 +0200

    remove subcommands as they're ugly

--- slash/linguistique/translitterate.js
+++ slash/linguistique/translitterate.js
@@ -5,79 +5,43 @@ module.exports = {
 	data: new SlashCommandBuilder()
         .setName('translitterate')
         .setDescription("permet de changer d'alphabet un mot ou une expression")
-        .addSubcommand(subcommand => 
-                subcommand.setName("lat_to_georg")
-                          .setDescription("translittère de l'alphabet latin vers l'alphabet georgien")
-                          .addStringOption(option => 
-                                option.setName('text')
-                                      .setDescription("le mot ou l'expression que vous souhaitez translittérer")
-                                      .setRequired(true)
-                            )
-            )
-        .addSubcommand(subcommand => 
-            subcommand.setName("lat_to_cyr")
-                      .setDescription("translittère de l'alphabet latin vers l'alphabet cyrillique")
-                      .addStringOption(option => 
-                            option.setName('text')
-                                  .setDescription("le mot ou l'expression que vous souhaitez translittérer")
-                                  .setRequired(true)
-                        )
-        )
-        .addSubcommand(subcommand => 
-            subcommand.setName("georg_to_lat")
-                      .setDescription("translittère de l'alphabet georgien vers l'alphabet latin")
-                      .addStringOption(option => 
-                            option.setName('text')
-                                  .setDescription("le mot ou l'expression que vous souhaitez translittérer")
-                                  .setRequired(true)
-                        )
-        )
-        .addSubcommand(subcommand => 
-            subcommand.setName("cyr_to_lat")
-                      .setDescription("translittère de l'alphabet cyrillique vers l'alphabet latin")
-                      .addStringOption(option => 
-                            option.setName('text')
-                                  .setDescription("le mot ou l'expression que vous souhaitez translittérer")
-                                  .setRequired(true)
-                        )
-        )
-        .addSubcommand(subcommand =>
-            subcommand.setName("kor_to_lat")
-                      .setDescription("translittère de l'alphabet coréen vers l'alphabet latin")
-                      .addStringOption(option =>
-                            option.setName('text')
-                                    .setDescription("le mot ou l'expression que vous souhaitez translittérer")
-                                    .setRequired(true)
+        .addStringOption(option =>
+                option.setName('langages')
+                      .setDescription("les langues disponibles sont: lat_to_georg, lat_to_cyr, georg_to_lat, cyr_to_lat, kor_to_lat, lat_to_kor")
+                      .setRequired(true)
+                      .addChoices(
+                        {name : "latin vers géorgien", value : "lat_to_georg"},
+                        {name : "latin vers cyrillique", value : "lat_to_cyr"},
+                        {name : "latin vers coréen", value : "lat_to_kor"},
+                        {name : "géorgien vers latin", value : "georg_to_lat"},
+                        {name : "cyrillique vers latin", value : "cyr_to_lat"},
+                        {name : "coréen vers latin", value : "kor_to_lat"}
                       )
-        )
-        .addSubcommand(subcommand =>
-            subcommand.setName("lat_to_kor")
-                      .setDescription("translittère de l'alphabet latin vers l'alphabet coréen")
-                      .addStringOption(option =>
-                            option.setName('text')
-                                  .setDescription("le mot ou l'expression que vous souhaitez translittérer")
-                                  .setRequired(true)
-                        )
+            )
+        .addStringOption(option =>
+                option.setName('text')
+                        .setDescription("le texte à translittérer")
+                        .setRequired(true)
         ),
         
         async execute(interaction){
             let text = interaction.options.getString('text');
-            if (interaction.options.getSubcommand() === 'lat_to_georg'){
+            if (interaction.options.getString('langages') === 'lat_to_georg'){
                 await interaction.reply(data.translitterate.lat_to_georg(text));
             }
-            else if (interaction.options.getSubcommand() === 'lat_to_cyr'){
+            else if (interaction.options.getString('langages') === 'lat_to_cyr'){
                 await interaction.reply(data.translitterate.lat_to_cyr(text));
             }
-            else if (interaction.options.getSubcommand() === 'georg_to_lat'){
+            else if (interaction.options.getString('langages') === 'georg_to_lat'){
                 await interaction.reply(data.translitterate.georg_to_lat(text));
             }
-            else if (interaction.options.getSubcommand() == 'cyr_to_lat'){
+            else if (interaction.options.getString('langages') == 'cyr_to_lat'){
                 await interaction.reply(data.translitterate.cyr_to_lat(text));
             }
-            else if (interaction.options.getSubcommand() == 'kor_to_lat'){
+            else if (interaction.options.getString('langages') == 'kor_to_lat'){
                 await interaction.reply(data.translitterate.kor_to_lat(text));
             }
-            else if (interaction.options.getSubcommand() == 'lat_to_kor'){
+            else if (interaction.options.getString('langages') == 'lat_to_kor'){
                 await interaction.reply(data.translitterate.lat_to_kor(text));
             }
         }
